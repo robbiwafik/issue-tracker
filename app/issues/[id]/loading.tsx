c33ddedba3f3cb82280c77cc76e543b8ced8a797
Robbi Wafik <robbiwafik31@gmail.com>
2024-04-19 21:10:39 +0700

    Add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,6 +1,19 @@
+import { Box, Card } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 const LoadingIssueDetailPage = () => {
     return (
-        <div>Loading...</div>
+        <Box className='max-w-xl'>
+            <Skeleton />
+            <div className='space-x-3 my-4'>
+                <Skeleton width='5rem' inline />
+                <Skeleton width='8rem' inline />
+            </div>
+            <Card className='prose' my='6'>
+                <Skeleton count={3} />
+            </Card>
+        </Box>
     )
 }
 
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,6 +1,16 @@
+import { Box, Button } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 const LoadingNewIssuePage = () => {
     return (
-        <div>Loading...</div>
+        <Box className='max-w-xl'>
+            <div className='space-y-3'>
+                <Skeleton height={'2rem'} />
+                <Skeleton height={'20rem'} />
+                <Button disabled>Submit New Issue</Button>
+            </div>
+        </Box>
     )
 }
 
