53dcabfb3684affc4b4539feec12f24bd8a0a5a8
Robbi Wafik <robbiwafik31@gmail.com>
2024-04-23 14:00:01 +0700

    Improve the loading experience

---
+++ app/issues/_components/IssueFormSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Box, Button } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+
+const IssueFormSkeleton = () => {
+    return (
+        <Box className='max-w-xl'>
+            <div className='space-y-3'>
+                <Skeleton height={'2rem'} />
+                <Skeleton height={'20rem'} />
+                <Button disabled>Submit New Issue</Button>
+            </div>
+        </Box>
+    )
+}
+
+export default IssueFormSkeleton
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,16 +1,3 @@
-import { Skeleton } from '@/app/components'
-import { Box, Button } from '@radix-ui/themes'
+import IssueFormSkeleton from '../_components/IssueFormSkeleton'
 
-const LoadingNewIssuePage = () => {
-    return (
-        <Box className='max-w-xl'>
-            <div className='space-y-3'>
-                <Skeleton height={'2rem'} />
-                <Skeleton height={'20rem'} />
-                <Button disabled>Submit New Issue</Button>
-            </div>
-        </Box>
-    )
-}
-
-export default LoadingNewIssuePage
+export default IssueFormSkeleton
