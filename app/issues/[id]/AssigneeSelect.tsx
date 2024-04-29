6d6058f1089f7951e4e38bae81d3027339bd1386
Robbi Wafik <robbiwafik31@gmail.com>
2024-04-29 21:17:53 +0700

    Refactor the AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -4,6 +4,7 @@ import { Issue, User } from '@prisma/client'
 import { Select, Skeleton } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { useRouter } from 'next/navigation'
 import toast, { Toaster } from 'react-hot-toast'
 
 interface Props {
@@ -11,12 +12,8 @@ interface Props {
 }
 
 const AssigneeSelect = ({ issue }: Props) => {
-    const { data: users, error, isLoading } = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60 * 1000, //60s
-        retry: 3
-    })
+    const { data: users, error, isLoading } = useUsers()
+    const router = useRouter()
 
     if (isLoading)
         return <Skeleton height='32px' />
@@ -31,12 +28,14 @@ const AssigneeSelect = ({ issue }: Props) => {
             {assignedToUserId: userId === unassignedSelectValue ? null : userId}
         )
         .catch(() => toast.error('Changes could not be saved'))
+
+        router.refresh()
     }
  
     return (
         <>
             <Select.Root 
-                defaultValue={issue.assignedToUserId || ' '}
+                defaultValue={issue.assignedToUserId || unassignedSelectValue}
                 onValueChange={handleValueChange}
             >
                 <Select.Trigger placeholder='Assign...' />
@@ -59,5 +58,12 @@ const AssigneeSelect = ({ issue }: Props) => {
         </>
     )
 }
+
+const useUsers = () => useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3
+})
 
 export default AssigneeSelect
